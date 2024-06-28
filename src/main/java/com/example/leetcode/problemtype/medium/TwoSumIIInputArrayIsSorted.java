f54b6c2b15fc22800cc8bcf9ea59d35e58012304
baddanki <baddanki@vmware.com>
2024-06-28 21:39:55 +0530

    Added Medium problems

---
+++ src/main/java/com/example/leetcode/problemtype/medium/TwoSumIIInputArrayIsSorted.java
@@ -0,0 +1,63 @@
+package com.example.leetcode.problemtype.medium;
+
+/**
+ * Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+ * <p>
+ * Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+ * <p>
+ * The tests are generated such that there is exactly one solution. You may not use the same element twice.
+ * <p>
+ * Your solution must use only constant extra space.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: numbers = [2,7,11,15], target = 9
+ * Output: [1,2]
+ * Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: numbers = [2,3,4], target = 6
+ * Output: [1,3]
+ * Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
+ * <p>
+ * Example 3:
+ * <p>
+ * Input: numbers = [-1,0], target = -1
+ * Output: [1,2]
+ * Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
+ * <p>
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 2 <= numbers.length <= 3 * 104
+ * -1000 <= numbers[i] <= 1000
+ * numbers is sorted in non-decreasing order.
+ * -1000 <= target <= 1000
+ * The tests are generated such that there is exactly one solution.
+ * <p>
+ * Leetcode link : https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+ */
+public class TwoSumIIInputArrayIsSorted {
+
+    public int[] twoSum(int[] numbers, int target) {
+
+        int low = 0;
+        int high = numbers.length - 1;
+        while (low < high) {
+            int sum = numbers[low] + numbers[high];
+            if (sum == target) {
+                return new int[]{low+1, high+1};
+            } else if (sum < target) {
+                low = low + 1;
+            } else {
+                high = high - 1;
+            }
+        }
+        return new int[]{-1, -1};
+
+    }
+}
